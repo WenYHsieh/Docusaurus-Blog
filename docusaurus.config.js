4f4ea0616c2f66a28ed662642f6349a229c6ea2e
WenYHsieh <menlen6205@gmail.com>
2022-12-12 22:29:38 +0800

    [EDIT] fix domain name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 const config = {
   title: 'title',
   tagline: 'Dinosaurs are cool',
-  url: 'https://NoraYu-blog.netlify.app',
+  url: 'https://norayu-blog.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
